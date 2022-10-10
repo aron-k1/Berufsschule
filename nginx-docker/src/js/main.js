726c250f40df5b24de8c5dbd4de454a7b602338f
mopschond <Aron.kiesel@gmail.com>
2022-10-10 12:31:31 +0200

    finished bootstrap

---
+++ nginx-docker/src/js/main.js
@@ -0,0 +1,11 @@
+function changeCurrentActiveNav(anchor) {
+    var zutritt = document.getElementById("zutritt_nav");
+    zutritt.className = zutritt.className.replace(' active', '');
+    var contact = document.getElementById("contact_nav");
+    contact.className = contact.className.replace(' active', '');
+    var bank = document.getElementById("bank_nav");
+    bank.className = bank.className.replace(' active', '');
+    const currentElement = document.getElementById(anchor);
+    currentElement.className = currentElement.className + " active";
+
+}
